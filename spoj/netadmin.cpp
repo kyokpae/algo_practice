cef8dc3cb6c7862079ee7707c93c430cf7aaf515
Marcin Olak <marcin.olak@gmail.com>
2011-12-12 10:57:33 +0100

    SPOJ: NETADMIN (TLE)

--- spoj/netadmin.cpp
+++ spoj/netadmin.cpp
@@ -1,6 +1,5 @@
-// http://www.spoj.pl/problems/NETADMIN/
+// Graph
 #include <iostream>
-#include <set>
 #include <vector>
 
 using namespace std;
@@ -9,22 +8,108 @@ using namespace std;
 #define VAR(v, n) __typeof(n) v=(n)
 #define FOREACH(i, c) for ( VAR(i, (c).begin()); i != (c).end(); ++i)
 
-struct Edge {
-    Edge(int v) : v(v), used(false) {}
-    int v;
-    bool used;
-};
+template <int N> class Graph {
+public:
+    class Edge {
+    public:
+        Edge() : v(-1), c(0), u(0) {}
+        Edge(int v) : v(v), c(0), u(0) {}
+        int v;
+        //////////////////////////////////////
+        bool ok() {
+            return (c - u) > 0;
+        }
+        
+        void visit() {
+            //cout << "e" << endl;
+            u++;
+        }
 
-struct Vertex {
-    Vertex() : user(false), p(-1), e(NULL) {}
-    bool user;
-    int p;
-    Edge * e;
-    vector<Edge> E;
-};
+        int c, u;
+    };
+    
+    class Vertex {
+    public:
+        Vertex() : s(-1), f(-1), p(-1), user(false), connected(false) {}
+                
+        void reset(int c) {
+            f = s = p = -1; connected = false;
+            FOREACH(it, E) {
+                Edge& e = *it;
+                if (e.v == -1) {
+                    e.c = 1;
+                } else {
+                    e.c = c;
+                }
+            }
+        }
+        void clear() { E.clear(); reset(); }
+        int s, f, p;
+        vector<Edge> E;
+        ///////////////////////////////////////
+        void visit(int v) {
+            p = v;
+        }
 
-struct Graph {
-
+        bool user, connected;
+    };
+    
+    void edgeU(int from, int to) {
+        edgeD(from, to);
+        edgeD(to, from);
+    }
+    
+    void edgeD(int from, int to) {
+        V[from].E.push_back(Edge(to));
+    }
+    
+    void dfs(int n) {
+        int t = 0;
+        REP(i, n) {
+            if (V[i].s == -1) {
+                dfsVisit(i, t);
+            }
+        }
+    }
+    
+    int maxFlow(int n) {
+        int flow = 0;
+        while (true) {
+            REP(i, n) {
+                V[i].s = -1;
+            }
+            int t = 0;
+            if (!dfsVisit(0, t)) {
+                break;
+            }
+            flow++;
+        }
+        return flow;
+    }    
+    
+    bool dfsVisit(int i, int& t) {
+        //cout << "dfs " << i << endl;
+        V[i].s = ++t;
+        FOREACH(it, V[i].E) {
+            Edge& e = *it;
+            if (e.v == -1) {
+                //cout << "reached sink" << endl;
+                e.visit();
+                return true;
+            } else            
+            if (V[e.v].s == -1 && e.ok()) {
+                V[e.v].visit(i);
+                int ret = dfsVisit(e.v, t);
+                if (ret) {
+                    e.visit();
+                    return true;
+                }
+            }
+        }
+        return false;
+        V[i].f = ++t;
+    }
+    
     void solve() {
         int n, m, users;
         cin >> n >> m >> users;
@@ -33,79 +118,57 @@ struct Graph {
             int h;
             cin >> h;
             V[--h].user = true;
+            edgeD(h, -1);
         }
         // read streets
         REP(i, m) {
             int u, v;
             cin >> u >> v;
             edgeU(--u, --v);
-        }
+        }        
+        int l = 1;
+        //int m = 250;
+        int r = 499;
+        //cout << "SOlve" << endl;
         int flow = 0;
-        while (true) {
-            int q[500];
-            int b = 0;
-            int e = 0;
-            int found = -1;
+        while (l != r) {
             REP(i, n) {
-                V[i].p = -1;
-                V[i].e = NULL;
+                V[i].reset(l);
             }
-            q[0] = 0;
-            V[0].p = 0;
-            while (b <= e && found == -1) {
-                int v = q[b++];
-                //cout << v << " " << colors << endl;
-                FOREACH(it, V[v].E) {
-                    Edge& edge = *it;
-                    if (!edge.used && V[edge.v].p == -1 && edge.v > 0) {
-                        found = edge.v;
-                        V[found].p = v;
-                        V[found].e = &edge;                        
-                        if (V[found].user) {
-                            V[found].user = false;
-                            flow++;
-                            //cout << "Found " << found << endl;
-                            break;
-                        } else {
-                            q[++e] = found;
-                            found = -1;
-                        }
-                    }
-                }
-            }
-            if (found == -1) {
+            flow += maxFlow(n);
+            
+            //cout << "c: " << l << ", flow: " << flow << endl; 
+            if (flow == users) {
+                cout << l << endl;
                 break;
-            } else {
-                //cout << "Found user in " << found << ". Connecting " << endl;
-                while (found) {
-                    //cout << "F " << found << endl;
-                    //cout << "C " << V[found].c << endl;
-                    V[found].e->used = true;
-                    found = V[found].p;
-                    //cout << found << " " << endl;
-                }
-                //cout << endl;
             }
-        }
-        int colors = (users / flow) + ((users % flow > 0) ? 1 : 0);
-        cout << colors << endl;
+            l++;
+            /*
+            if (result < h) {
+                l = m;
+                m = (l+r) / 2;
+            } else {
+                if (result == h) {
+                    cout << m << endl;
+                    break;
+                }
+                if (result >= h) {
+                    r = m;
+                    m = (l+r) / 2;
+                }
+            }*/
+        }        
     }
-
-    void edgeU(int u, int v) {
-        V[u].E.push_back(Edge(v));
-        V[v].E.push_back(Edge(u));
-    }
-
-    Vertex V[500];
+    
+    Vertex V[N];    
 };
 
-int main() {
+int main(void) {
     int t;
     cin >> t;
     while (t--) {
-        Graph g;
-        g.solve();
+        Graph<500> g;
+        g.solve();        
     }
     return 0;
 }
-
